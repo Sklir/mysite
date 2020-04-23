b0d06c4d43c32159d68dcca0c3f18ff645719c64
Sklir <admin@admin.com>
2020-04-23 12:07:49 +0300

    Slim

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,10 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
+def slim(request):
+    return render(request, 'blog/slim')
+
+
 
 
 
