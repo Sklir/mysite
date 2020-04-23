8cbf0463b3c2a84cc23fc5f6b3d7fb17d0c3890b
Sklir <admin@admin.com>
2020-04-23 13:52:07 +0300

    Pages

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('slim', views.slim, name='slim')
+    path('', views.main, name='post_list'),
+    path('slim', views.slim, name='slim'),
+    path('mass', views.mass, name='mass'),
+    path('crossfit', views.crossfit, name='crossfit'),
 ]
